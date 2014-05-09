49e0c34c26c084a70b15cf1ef239597e98b895f5
Pal Konyves <paul.konyves@gmail.com>
2014-05-09 22:00:14 +0100

    pom.xml, eclipse kepler project

---
+++ src/main/java/hu/palkonyves/App.java
@@ -0,0 +1,13 @@
+package hu.palkonyves;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
